e6b50fc31b6301362354aef95743ac66750e1ff8
darklaw53 <marceloschulze@gmail.com>
2023-05-20 10:22:45 -0300

    playerMovemeantAdded

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    bool lookingRight;
+    float currentSpeed;
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        currentSpeed = movement.sqrMagnitude;
+    }
+
+    private void FlipPlayer()
+    {
+        lookingRight = !lookingRight;
+        if (!lookingRight)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        movement = movement.normalized;
+        rb.velocity = movement * moveSpeed;
+
+        if (currentSpeed > 0)
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+
+        if ((movement.x > 0 && !lookingRight) || (movement.x < 0 && lookingRight))
+        {
+            FlipPlayer();
+        }
+    }
+}
